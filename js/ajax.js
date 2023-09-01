515d947e4211f7fd60bf64f8b6d2a1f9db2d874f
mbgh2 <95969022+mbgh2@users.noreply.github.com>
2023-09-01 23:10:31 +0300

    Add files via upload

--- js/ajax.js
+++ js/ajax.js
@@ -1,1 +1,13 @@
 
+let xhttp = new XMLHttpRequest();
+xhttp.open('post', 'back.php', true);
+xhttp.setRequestHeader('Content-type', "application/x-www-form-urlencoded");
+
+xhttp.send('name = vasya');
+
+xhttp.onreadystatechange = function(){
+   if(this.readyState == 4 && this.status == 200) {
+      console.log(this);
+   } 
+}
+
